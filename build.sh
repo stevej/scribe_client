ae8fb09c6576a542c18c01c1d9c6cf6014919bb4
Eric Jensen <ej@twitter.com>
2010-08-10 15:14:14 -0700

    upgrade again

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=0.1.0-thrift-r982827
+VERSION=0.1.0-thrift-r983680
 
 rm -rf gen-java
 
@@ -10,7 +10,7 @@ thrift --gen java src/main/thrift/scribe.thrift
 rm -rf target
 mkdir target
 
-javac -classpath lib/libthrift-0.4.0-instant-r982827.jar:lib/slf4j-api-1.5.8.jar -d target `find gen-java -name \*.java -print`
+javac -classpath lib/libthrift-0.4.0-instant-r983680.jar:lib/slf4j-api-1.5.8.jar -d target `find gen-java -name \*.java -print`
 
 cd target
 jar cvf ../scribe_client-$VERSION.jar *
