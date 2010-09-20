e0bffacc35c1b750f8b75d4368960c5def7557bd
John Corwin <jcorwin@twitter.com>
2010-09-20 14:58:38 -0700

    upgrade to libthrift-0.4.0

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ thrift --gen java src/main/thrift/scribe.thrift
 rm -rf target
 mkdir target
 
-javac -classpath lib/libthrift-0.2.0.jar:lib/slf4j-api-1.5.8.jar -d target `find gen-java -name \*.java -print`
+javac -classpath lib/libthrift-0.4.0.jar:lib/slf4j-api-1.5.8.jar -d target `find gen-java -name \*.java -print`
 
 cd target
 jar cvf ../scribe_client-$VERSION.jar *
